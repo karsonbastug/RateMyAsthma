5f897afc3069caf0e7f1602518d60cfa2e01003b
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-03-18 21:35:17 -0400

    Add project files.

---
+++ RateMyAsthma/Pages/AMain.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RateMyAsthma.Pages
+{
+    public class AMainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RateMyAsthma/Pages/AToday.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RateMyAsthma.Pages
+{
+    public class ATodayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

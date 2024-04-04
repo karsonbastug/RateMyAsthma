93b0afd1bf76be0edc85a88edd62d8f929cacad9
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-04-04 11:01:29 -0400

    Updated Today page

---
+++ RateMyAsthma/Pages/APast.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RateMyAsthma.Pages
+{
+    public class APastModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RateMyAsthma/Pages/AReview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RateMyAsthma.Pages
+{
+    public class AReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

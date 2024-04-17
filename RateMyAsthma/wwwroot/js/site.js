6fb8718e296e96e972e3cad835e318cc0bb38b6f
cb00059 <cb00059@mix.wvu.edu>
2024-04-17 01:23:30 -0400

    Working Max Search
    Returns top 5: I fixed someones sp too make sure its updated

--- RateMyAsthma/wwwroot/js/site.js
+++ RateMyAsthma/wwwroot/js/site.js
@@ -3,17 +3,7 @@
 
 // Write your JavaScript code. on click button, C#
 
-function getRandomTemperature(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-document.getElementById('mondayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('tuesdayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('wednesdayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('thursdayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('fridayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('saturdayTemp').textContent = getRandomTemperature(67, 77);
-document.getElementById('sundayTemp').textContent = getRandomTemperature(67, 77);
 async function displayCityReviews(City) {
     const response = await fetch(`https://localhost:7070/api/CityReview/${City}`);
     const data = await response.json();
@@ -62,23 +52,47 @@ async function maxTempSearch(MaxTemp) {
     const response = await fetch(`https://localhost:7070/api/MaxHealth/${MaxTemp}`);
     const data = await response.json();
     document.getElementById('cityM').textContent = data[0].city;
-    document.getElementById('cityM').style.visibility = "visible";
-    document.getElementById('cityTempM').textContent = data[0].maxTemp;
-    document.getElementById('cityTempM').style.visibility = "visible";
-    document.getElementById('cityRatingM').textContent = data[0].rating;
-    document.getElementById('cityRatingM').style.visibility = "visible";
     document.getElementById('cityCommentM').textContent = data[0].comment;
-    document.getElementById('cityCommentM').style.visibility = "visible";
+    document.getElementById('cityRatingM').textContent = data[0].rating;
+    document.getElementById('cityTempM').textContent = data[0].maxTemp;
     document.getElementById('cityDateM').textContent = data[0].date;
-    document.getElementById('cityDateM').style.visibility = "visible";
+    document.getElementById('HighTempDisplayBox').style.display = 'inline-block';
+
+    document.getElementById('cityM1').textContent = data[1].city;
+    document.getElementById('cityCommentM1').textContent = data[1].comment;
+    document.getElementById('cityRatingM1').textContent = data[1].rating;
+    document.getElementById('cityTempM1').textContent = data[1].maxTemp;
+    document.getElementById('cityDateM1').textContent = data[1].date;
+
+    document.getElementById('HighTempDisplayBox1').style.display = 'inline-block';
+
+    document.getElementById('cityM2').textContent = data[1].city;
+    document.getElementById('cityCommentM2').textContent = data[1].comment;
+    document.getElementById('cityRatingM2').textContent = data[1].rating;
+    document.getElementById('cityTempM2').textContent = data[1].maxTemp;
+    document.getElementById('cityDateM2').textContent = data[1].date;
+
+    document.getElementById('HighTempDisplayBox2').style.display = 'inline-block';
+
+    document.getElementById('cityM3').textContent = data[2].city;
+    document.getElementById('cityCommentM3').textContent = data[2].comment;
+    document.getElementById('cityRatingM3').textContent = data[2].rating;
+    document.getElementById('cityTempM3').textContent = data[2].maxTemp;
+    document.getElementById('cityDateM3').textContent = data[2].date;
+
+    document.getElementById('HighTempDisplayBox3').style.display = 'inline-block';
+
+    document.getElementById('cityM4').textContent = data[3].city;
+    document.getElementById('cityCommentM4').textContent = data[3].comment;
+    document.getElementById('cityRatingM4').textContent = data[3].rating;
+    document.getElementById('cityTempM4').textContent = data[3].maxTemp;
+    document.getElementById('cityDateM4').textContent = data[3].date;
+
+    document.getElementById('HighTempDisplayBox4').style.display = 'inline-block';
 
 
-    const reviewDate = new Date(data[0].date);
 
-    const formattedDate = reviewDate.toLocaleDateString();
 
-    document.getElementById('cityDateM').textContent = formattedDate;
-    document.getElementById('cityDateM').style.visibility = "visible";
 }
 function submitHighTemp() {
     var highTempValue = document.getElementById('highTemp').value;
